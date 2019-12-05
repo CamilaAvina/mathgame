72298786a05f3a88c6c2a376d31f948f1a69bf2d
Camila Avina <twin2avina@gmail.com>
2019-12-05 14:58:10 -0800

    snake starter pack

--- sketch.js
+++ sketch.js
@@ -1,128 +1,33 @@
-/***** ELEMENTS *****/
-var startButton = document.getElementById("start");
-var stopButton = document.getElementById("stop");
-var inputField = document.getElementById("in");
-var form = document.querySelector("form");
-var p = document.getElementById("p");
-var q = document.getElementById("q");
-var op = document.getElementById("op");
-var response = document.getElementById("response"); // used for Try Again text
-var results = document.getElementById("results");
-var category = document.getElementById("category");
+let s;
+let scl = 20;
 
-/***** STATE VARIABLES *****/
-var max = 20;
-var num1;
-var num2;
-var answer;
+function setup() {
+	createCanvas(600,600);
+s = new Snake();
+frameRate(10);
 
-var startTime;
-var endTime;
+}
 
-var count; // number of correct answers
-var times = [];
 
-/***** INITIALIZING *****/
-inputField.className = "hide";
-stopButton.className = "hide";
+function draw(){
+	background(51);
+	s.update();
+	s.show();
+}
 
-/***** EVENTS *****/
-startButton.onclick = function() {
-	// initializing the count
-	count = 0;
-	times = [];
-	results.innerHTML = ""; // clear results
-	category.innerHTML = ""; // clear category
-	refreshNums();
-	inputField.className = ""; // show the input field
-	stopButton.className = ""; // show the stop button
-	startButton.className = "hide"; // hide the start button
-	inputField.focus();
-};
 
-form.onsubmit = function(e) {
-	// need to prevent the default form submission wich reloads the page
-	e.preventDefault();
-	getAnswer();
-};
-
-stopButton.onclick = function() {
-	var resultString;
-	var categoryString;
-	if (times.length > 0) {
-		// getting mean time
-		var total = 0;
-		for (var i = 0; i < times.length; i++) {
-			total += times[i];
-		}
-		var mean = (total / times.length) / 1000;
-		resultString = "Average time: " + mean.toPrecision(4) + " sec";
-		categoryString = getCategory(mean);
-	} else {
-		resultString = "No results recorded. Hit the Enter key to submit your answers.";
-		categoryString = "";
+function keyPressed(){
+	if (keyCode === UP_ARROW) {
+		s.dir(0,-1);
 	}
-
-	inputField.className = "hide"; // hide the input field
-	stopButton.className = "hide"; // hide the stop button
-	startButton.className = ""; // show the start button
-
-	// clear numbers and present results
-	p.innerHTML = "";
-	q.innerHTML = "";
-	op.innerHTML = "";
-	response.innerHTML = ""; // clear response in case it was set
-	results.innerHTML = resultString;
-	category.innerHTML = categoryString;
-};
-
-/***** FUNCTIONS ******/
-var refreshNums = function() {
-	// Getting some random numbers
-	num1 = Math.floor((Math.random() * max) + 1);
-	num2 = Math.floor((Math.random() * max) + 1);
-	// Printing numbers to user
-	p.innerHTML = num1;
-	op.innerHTML = "+";
-	q.innerHTML = num2;
-	// Starting timer
-	startTime = new Date();
-};
-
-/*
-* This is called in the onsubmit event
-*/
-var getAnswer = function() {
-	var correct = num1 + num2;
-	// Getting the users attempt
-	answer = parseInt(inputField.value);
-
-	if (answer === correct) {
-		// Stopping the timer and adding the time to the times array
-		endTime = new Date();
-		times[count++] = endTime.getTime() - startTime.getTime();
-		// the answer was correct, so no need for "Try Again"
-		response.innerHTML = "";
-		refreshNums();
-	} else {
-		response.innerHTML = "Try Again";
+		else if (keyCode === DOWN_ARROW) {
+				s.dir(0,1);
 	}
-	// clear the input field for the next round
-	inputField.value = "";
-};
+	else if (keyCode === RIGHT_ARROW) {
+			s.dir(1,0);
+}
+else if (keyCode === LEFT_ARROW) {
+		s.dir(-1,0);
+}
 
-var getCategory = function(mean) {
-	var c;
-	if (mean < 2) {
-		c = "Human Computer";
-	} else if (mean < 4) {
-		c = "Math Wiz";
-	} else if (mean < 7) {
-		c = "B Student";
-	} else if (mean < 10) {
-		c = "Probably Drunk";
-	} else {
-		c = "High School Drop Out";
-	}
-	return c;
-};
+}

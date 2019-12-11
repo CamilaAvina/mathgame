8238dd4ec9538b939fec6bfdef8ada49a2fa4874
Camila Avina <twin2avina@gmail.com>
2019-12-11 02:05:03 -0800

    tittle page and button

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 let s;
 let scl = 20;
+let state = 'title';
 let food;
 let question = ['3 + 2 =', '5 + 6 =', '2 + 8 =', '3 + 4 =', '2 + 4 ='];
 let randomQuest;
 let answers = ['5', '11', '10', '7', '6'];
 let randomAnsw;
+let button;
 
 
 
@@ -13,6 +15,12 @@ function setup() {
   s = new Snake();
   frameRate(3);
   pickLocation();
+  button = createButton('End Game');
+  button.mousePressed(title);
+  button.size(100,50);
+  button.style("background-color","#fc0000");
+  button.style("color","#fff");
+  button.position( 990,700);
 
 
 }
@@ -47,6 +55,57 @@ function pickLocation() {
 // }
 
 function draw() {
+  if (state=='title'){
+    title();
+  }
+  else if (state === 'level 1') {
+    level1();
+  }
+}
+  // background(51);
+  //
+  // if (s.eat(food)) {
+  //   pickLocation();
+  // }
+  //
+  // s.death();
+  // s.update();
+  // s.show();
+  //
+  // fill(255,0, 0);
+  // //  if (answers[0]) {
+  // // //  frameRate(.5);
+  // //   randomAnsw = int(random(answers.length));
+  // //   textSize(18);
+  // //
+  // // text(answers[randomAnsw],food.x,food.y,scl,scl);
+  // //  answers.splice(randomAnsw,1);
+  // // }
+  // // else {
+  // //   frameRate(5);
+  // //     rect(food.x, food.y, scl, scl);
+  // // }
+  //
+  // // rect(food.x, food.y, scl, scl);
+  //
+  // text(random(answers), food.x, food.y, scl, scl);
+  // equa();
+
+
+
+
+function title(){
+  background(0);
+  fill(255);
+  textSize(30);
+  text('Number Eating',200,150 );
+  textSize(18);
+  text('click to start',200,200);
+  text('use the arrows to move around',200,300);
+}
+
+
+function level1() {
   background(51);
 
   if (s.eat(food)) {
@@ -57,25 +116,15 @@ function draw() {
   s.update();
   s.show();
 
-  fill(255, 0, 100);
-  //  if (answers[0]) {
-  // //  frameRate(.5);
-  //   randomAnsw = int(random(answers.length));
-  //   textSize(18);
-  //
-  // text(answers[randomAnsw],food.x,food.y,scl,scl);
-  //  answers.splice(randomAnsw,1);
-  // }
-  // else {
-  //   frameRate(5);
-  //     rect(food.x, food.y, scl, scl);
-  // }
-
-  // rect(food.x, food.y, scl, scl);
-
+  fill(255,0, 0);
   text(random(answers), food.x, food.y, scl, scl);
   equa();
 
+}
+
+
+function mousePressed(){
+  state = 'level 1'
 }
 
 
@@ -93,7 +142,7 @@ function keyPressed() {
 }
 
 function equa() {
-delayTime(delayTime);
+// delayTime(delayTime);
   if (question[0]) {
     //  frameRate(.5);
     randomQuest = int(random(question.length));
@@ -105,4 +154,11 @@ delayTime(delayTime);
   } else {
     print('your done');
   }
+}
+
+
+
+
+function titleMouseClicked(){
+  state = 'level 1';
 }
--- snake.js
+++ snake.js
@@ -50,7 +50,7 @@ function Snake() {
   }
 
   this.show = function() {
-    fill(255)
+    fill(random(255),random(255),random(255));
 		for (let i = 0; i < this.total; i++) {
 			  rect(this.tail[i].x, this.tail[i].y, scl, scl);
 
